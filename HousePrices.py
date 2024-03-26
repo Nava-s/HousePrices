3dd947216f6abf0dc1d5e2a5bfd054dab4311822
Antonio Proietti <65329472+Nava-s@users.noreply.github.com>
2024-03-26 16:08:51 +0100

    Update HousePrices.py

--- HousePrices.py
+++ HousePrices.py
@@ -1,15 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import scale
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import make_scorer, mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
 
-#import the dataset
+
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
@@ -92,45 +97,71 @@ one_hot_combined = pd.get_dummies(combined, drop_first=True)
 one_hot = one_hot_combined.iloc[:len(train)]
 one_hot_test = one_hot_combined.iloc[len(train):]
 
-
-
-
-lr = LinearRegression()
 scaler = StandardScaler()
-train[qt_index] = scaler.fit_transform(train[qt_index])
-test[qt_index_test] = scaler.fit_transform(test[qt_index_test])
+scaler.fit(train[qt_index])
+train[qt_index] = scaler.transform(train[qt_index])
+test[qt_index_test] = scaler.transform(test[qt_index_test])
+
 train = train.drop(ql_index, axis = 1)
 test = test.drop(ql_index_test, axis = 1)
+
 train = pd.concat([train, one_hot], axis = 1)
 test = pd.concat([test,one_hot_test], axis = 1)
+
 X = train[train.columns]
 X_submission = test[test.columns]
+
 X = X.drop(['SalePrice','Id'], axis = 1)
 X_submission = X_submission.drop(['Id'], axis = 1)
+
 y = train['SalePrice']
 
 #train test split
 X_train, X_test, y_train, y_test  = train_test_split(X,y,test_size=0.3,shuffle=True,random_state=123)
 
-#Introducing SBS 
-sfs = SequentialFeatureSelector(lr,
-                                n_features_to_select=100)
+# Definisci il modello della rete neurale
+model = Sequential([
+    Dense(64, activation='relu', input_shape=(232,)),
+    Dense(64, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(1, activation='linear')
+])
 
-sfs.fit(X,y)
-features = sfs.get_feature_names_out()
+# Compila il modello
+model.compile(optimizer=Adam(learning_rate=0.001),
+              loss='mse',  # Mean Squared Error per il problema di regressione
+              metrics=['mse'])  # Possiamo anche monitorare l'MSE durante l'addestramento
 
-X_train = X_train[features]
-X_test = X_test[features]
-X_submission = X_submission[features]
+# Addestra il modello
+history = model.fit(X_train, y_train, epochs=100, batch_size=None, validation_split=0.2, verbose=1)
 
-lr.fit(X_train,y_train)
-predictions = lr.predict(X_train)
-train_score = lr.score(X_train,y_train)
-test_score = lr.score(X_test,y_test)
-print(f'train score is {train_score:.2f} \n test score is {test_score:.2f}')
-X = X[features]
-lr.fit(X,y)
-y_predicted_sub = lr.predict(X_submission)
+# Valuta il modello
+train_loss, train_mse = model.evaluate(X_train, y_train, verbose=0)
+test_loss, test_mse = model.evaluate(X_test, y_test, verbose=0)
+print(f'Train MSE: {train_mse:.2f}')
+print(f'Test MSE: {test_mse:.2f}')
 
-submission_df = pd.DataFrame({'Id': test['Id'], 'SalePrice': y_predicted_sub})
-submission_df.to_csv('submission.csv', index=False,sep = ',')
+# Eseguire le predizioni sul dataset di test
+predictions_train_nn = model.predict(X_train)
+predictions_test_nn = model.predict(X_test)
+
+# Calcola R^2 per il set di addestramento
+r2_train_nn = 1 - (np.sum(np.square(y_train.values - predictions_train_nn.flatten())) / np.sum(np.square(y_train.values - np.mean(y_train))))
+print(f'R^2 sul set di addestramento: {r2_train_nn:.2f}')
+
+# Calcola R^2 per il set di test
+r2_test_nn = 1 - (np.sum(np.square(y_test.values - predictions_test_nn.flatten())) / np.sum(np.square(y_test.values - np.mean(y_test))))
+print(f'R^2 sul set di test: {r2_test_nn:.2f}')
+
+# Esegui le predizioni sul dataset di sottomissione X_submission
+y_predicted_sub = model.predict(X_submission)
+
+# Appiattisci la matrice 2D in un array 1D
+y_predicted_sub_flat = y_predicted_sub.flatten()
+
+# Crea un DataFrame per le predizioni di sottomissione
+submission_df = pd.DataFrame({'Id': test['Id'], 'SalePrice': y_predicted_sub_flat})
+
+# Salva il DataFrame come file CSV di sottomissione
+submission_df.to_csv('submission.csv', index=False, sep=',')
